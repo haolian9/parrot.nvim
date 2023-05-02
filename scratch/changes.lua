b55280e5667cadc1eb7aedb1831077d4a9b70a3a
haoliang <haoliang0.1.2@gmail.com>
2023-05-02 22:09:26 +0800

    fixup! scratch

--- scratch/changes.lua
+++ scratch/changes.lua
@@ -1,14 +1,3 @@
--- 目的：监测某个区间行的动态变化
---
--- 具体实现：
--- * 首尾行内的新增、删除行 -> 记录
--- * 首尾行外的变化 -> 忽略
---
--- 其他情况
--- * 改动区间与本区间
-
--- delete one line: {}
-
 local api = vim.api
 local tail = require("infra.tail")
 local fn = require("infra.fn")
@@ -17,40 +6,37 @@ local fn = require("infra.fn")
 ---@field private orig_start number
 ---@field private orig_last number
 ---@field private now_last number
+---@field op string
+---@field affected_lines number
 local BufLineEventInterpreter = {}
 do
-  function BufLineEventInterpreter:op()
-    local n = self.now_last - self.orig_last
-    if n == 0 then
-      return "noop"
-    elseif n > 0 then
-      return "add"
-    else
-      return "del"
-    end
-  end
-  ---@return number
-  function BufLineEventInterpreter:affected_lines() return math.abs(self.now_last - self.orig_last) end
   -- lines are 0-based
   -- left inclusive, right exclusive
-  function BufLineEventInterpreter:added_line_range()
-    assert(self:op() == "add", "unreachable")
+  function BufLineEventInterpreter:orig_range() return self.orig_start, self.orig_last end
+  -- lines are 0-based
+  -- left inclusive, right exclusive
+  function BufLineEventInterpreter:now_range() return self.orig_start, self.now_last end
+
+  -- lines are 0-based
+  -- left inclusive, right exclusive
+  function BufLineEventInterpreter:added_range()
+    assert(self.op == "add", "unreachable")
     return self.orig_last, self.now_last
   end
   -- lines are 0-based and in asc order
   function BufLineEventInterpreter:added_lines()
-    assert(self:op() == "add", "unreachable")
+    assert(self.op == "add", "unreachable")
     return fn.range(self.orig_last, self.now_last)
   end
   -- lines are 0-based
   -- left inclusive, right exclusive
-  function BufLineEventInterpreter:deleted_line_range()
-    assert(self:op() == "del", "unreachable")
+  function BufLineEventInterpreter:deleted_range()
+    assert(self.op == "del", "unreachable")
     return self.now_last, self.orig_last
   end
   -- lines are 0-based and in asc order
   function BufLineEventInterpreter:deleted_lines()
-    assert(self:op() == "del", "unreachable")
+    assert(self.op == "del", "unreachable")
     return fn.range(self.now_last, self.orig_last)
   end
 
@@ -58,7 +44,27 @@ do
   ---@return BufLineEventMaster
   function BufLineEventInterpreter.new(...)
     local _, _, _, orig_start, orig_last, now_last = ...
-    return setmetatable({ orig_start = orig_start, orig_last = orig_last, now_last = now_last }, { __index = BufLineEventInterpreter })
+
+    local n = now_last - orig_last
+
+    local op
+    if n == 0 then
+      op = "nochange"
+    elseif n > 0 then
+      op = "add"
+    else
+      op = "del"
+    end
+
+    local affected_lines = math.abs(n)
+
+    return setmetatable({
+      orig_start = orig_start,
+      orig_last = orig_last,
+      now_last = now_last,
+      op = op,
+      affected_lines = affected_lines,
+    }, { __index = BufLineEventInterpreter })
   end
 end
 
@@ -66,6 +72,8 @@ local bufnr = api.nvim_get_current_buf()
 
 vim.wo.list = true
 vim.wo.listchars = "tab:> ,trail:-,nbsp:+,eol:j"
+
+assert(api.nvim_buf_line_count(bufnr) >= 10, "need a larger buffer")
 
 local fpath = "/tmp/nvim-line-changes.log"
 tail.split_below(fpath)
@@ -76,21 +84,20 @@ api.nvim_buf_attach(bufnr, false, {
     local interpret = BufLineEventInterpreter.new(...)
     local _, _, tick, orig_start, orig_last, now_last = ...
 
-    local op = interpret:op()
     local lines = {}
     local line_range = {}
-    if op == "add" then
-      line_range = { interpret:added_line_range() }
+    if interpret.op == "add" then
+      line_range = { interpret:added_range() }
       lines = fn.concrete(interpret:added_lines())
-    elseif op == "del" then
-      line_range = { interpret:deleted_line_range() }
+    elseif interpret.op == "del" then
+      line_range = { interpret:deleted_range() }
       lines = fn.concrete(interpret:deleted_lines())
     else
       line_range = { "n/a" }
       lines = { "n/a" }
     end
 
-    file:write(string.format("tick=%d origin=[%d, %d) now=[%d, %d) op=%s n=%d range=%s lines=%s\n", tick, orig_start, orig_last, orig_start, now_last, op, interpret:affected_lines(), table.concat(line_range, "~"), table.concat(lines, ",")))
+    file:write(string.format("tick=%d origin=[%d, %d) now=[%d, %d) op=%s n=%d range=%s lines=%s\n", tick, orig_start, orig_last, orig_start, now_last, interpret.op, interpret.affected_lines, table.concat(line_range, "~"), table.concat(lines, ",")))
     file:flush()
   end,
 })
--- scratch/placeholder.lua
+++ scratch/placeholder.lua
@@ -1,14 +1,30 @@
--- todo: monitor reg`/` change
--- todo: aware of `\%V`
 local api = vim.api
 
 local bufnr = api.nvim_get_current_buf()
 local ns = api.nvim_create_namespace("sss")
+-- for: `$0`, `${0}`, `${0:zero}`
 local matcher = vim.regex([[\v(\$\d+)|(\$\{\d+(:[^}]+)?\})]])
 
 api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
 
-for i = 0, api.nvim_buf_line_count(bufnr) - 1 do
-  local col_start, col_stop = matcher:match_line(bufnr, i)
-  if col_start ~= nil then api.nvim_buf_add_highlight(bufnr, ns, "Search", i, col_start, col_stop) end
+---@type {[number]: number[]}
+local matches = {}
+for lnum = 0, api.nvim_buf_line_count(bufnr) - 1 do
+  matches[lnum] = {}
+  local offset = 0
+  while true do
+    local col_start, col_stop
+    do
+      local rel_start, rel_stop = matcher:match_line(bufnr, lnum, offset)
+      if rel_start == nil then break end
+      col_start = rel_start + offset
+      col_stop = rel_stop + offset
+    end
+    offset = col_stop
+    api.nvim_buf_add_highlight(bufnr, ns, "Search", lnum, col_start, col_stop)
+    table.insert(matches[lnum], col_start)
+    table.insert(matches[lnum], col_stop)
+  end
 end
+
+vim.notify(vim.inspect(matches))
