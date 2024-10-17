9d6ecaa645434d44f0c191b4c5f5bede899f39f7
haoliang <haoliang0.1.2@gmail.com>
2024-10-17 16:08:49 +0800

    sync with upstream

--- lua/parrot/facts.lua
+++ lua/parrot/facts.lua
@@ -1,10 +1,11 @@
 local M = {}
 
 local fs = require("infra.fs")
+local mi = require("infra.mi")
 local ni = require("infra.ni")
 
 ---@diagnostic disable-next-line: param-type-mismatch
-M.user_root = fs.joinpath(vim.fn.stdpath("config"), "chirps")
+M.user_root = fs.joinpath(mi.stdpath("config"), "chirps")
 
 M.anchor_ns = ni.create_namespace("parrot:anchors")
 
